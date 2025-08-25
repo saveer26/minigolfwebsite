2c1e5b855a4c4d960ecb7d06560bcac4c0b0ec22
saveer <saveer.patel26@gmail.com>
2025-08-25 19:28:54 -0400

    worked on the gallery

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
 function Alert(){
     alert("This isn't a real website. This is a demo. How did you even get on here?")
 }
+
+let gallery = ["images/gallery/galleryImage1.png","images/gallery/galleryImage2.png","images/gallery/galleryImage3.png"]
+let current = 1
+let image = document.getElementById("img")
+image.src = gallery[current]
+function previous() {
+    current-=1
+    if(current<0){
+        current=gallery.length-1
+    }
+    image.src=gallery[current]
+}
+function next() {
+    current+=1
+    if(current>gallery.length-1){
+        current=0
+    }
+    image.src=gallery[current]
+}

5238c109efef1d46b0f896bccab2f7a7cfe244e7
saveer <saveer.patel26@gmail.com>
2025-09-22 19:26:04 -0400

    ended project

--- gameScript.js
+++ gameScript.js
@@ -5,24 +5,31 @@ canvas.width = 500;
 canvas.height = 500;
 
 let ball = {
-   x:0,
-   y:0,
-
+   x:100,
+   y:100,
+   nextX:100,
+   nextY:100
 }
 function drawBall(){
 ctx.fillStyle = "white"
 ctx.beginPath()
-ctx.arc(ball.x,ball.y,5,0,2 * Math.PI)
+ctx.arc(ball.x,ball.y,10,0,2 * Math.PI)
 ctx.stroke()
 ctx.fill()
 }
 
-function moveBall(x,y){
-   ball.x = x
-   ball.y = y
-}
+function moveBall(){
+   if(ball.x != ball.nextX && ball.y != ball.nextY){
+    ball.x += 1
+    ball.y -= 1
 
+   }
+}
+function background(){
+    ctx.clearRect(0,0,canvas.width,canvas.height)
+}
 function update(){
+    background();
     drawBall();
     moveBall();
     requestAnimationFrame(update);
@@ -32,8 +39,8 @@ canvas.addEventListener('click', function(event) {
     const rect = canvas.getBoundingClientRect();
     const x = event.clientX - rect.left;
     const y = event.clientY - rect.top;
-    moveBall(x,y)
-
+    ball.nextX = x;
+    ball.nextY = y;
     console.log("Clicked at x: " + x + ", " + y);
 });
 
